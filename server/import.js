7cd3fcd93ccb425df7e142e07590b8aa0594d53b
HunterNL <hunternl@users.noreply.github.com>
2016-09-08 19:27:51 +0200

    fix card importer

--- server/import.js
+++ server/import.js
@@ -1,30 +1,49 @@
 function importCards(url) {
-	var returns = HTTP.get(url);
-	var data = returns.data;
-	if(!data) {
-		throw new Meteor.Error("cahdb_httP_no_data","No data found at url",url,returns);
+	var result = HTTP.get(url);
+	// var data = returns.data;
+	// if(!data) {
+	// 	throw new Meteor.Error("cahdb_httP_no_data","No data found at url",url,returns);
+	// }
+	if(result.statusCode !== 200) {
+		throw new Meteor.Error("import_invalid_code","server responded with statuscode "+result.statusCode+" ,expected 200");
 	}
+	
+	var content = result.content;
+	
+	if(!content) {
+		throw new Meteor.Error("import_no_data","server did not return data");
+	}
+	
+	var cardData = JSON.parse(content);
+	
+	if(!cardData) {
+		throw new Meteor.Error("import_parse_error","Could not parse card json");
+	}
+	
+	
 
 	//Strip out list of cards
-	var blackcards = data.blackCards;
-	var whitecards = data.whiteCards;
-	var order = data.order;
+	var blackcards = cardData.blackCards;
+	var whitecards = cardData.whiteCards;
+	var order = cardData.order;
 
-	delete data.blackCards;
-	delete data.whiteCards;
-	delete data.order;
-	//data is now just an object with carddsets
+	delete cardData.blackCards;
+	delete cardData.whiteCards;
+	delete cardData.order;
+	//cardData is now just an object with carddsets
 
-	//Clear the database
+	//Clear the cardDatabase
 	Cards.remove({});
 	CardSets.remove({});
 
 	//Can take a while, lets show a message
 	console.info("Now importing cards, this can take a minute!");
 
-	for(var key in data) {
-		if(data.hasOwnProperty(key)) {
-			var cardset = data[key];
+	for(var key in cardData) {
+		if(cardData.hasOwnProperty(key)) {
+			var cardset = cardData[key];
+			
+			console.log(cardset);
 
 			var cardset_black = cardset.black;
 			var cardset_white = cardset.white;
